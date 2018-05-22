dee26d42b5d16f1a8ba745ad0c8c1dabaa60360d
reneeb <info@perl-services.de>
2018-05-22 15:53:43 +0200

    handle new syntax to define routes with specific values

--- lib/Mojolicious/Plugin/NoIndex.pm
+++ lib/Mojolicious/Plugin/NoIndex.pm
@@ -6,7 +6,11 @@ use Mojo::Base 'Mojolicious::Plugin';
 
 use Carp qw(croak);
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
+
+my %routes;
+my %routes_special;
+
 
 sub register {
     my ($self, $app, $config) = @_;
@@ -17,17 +21,37 @@ sub register {
 
     my $default_value = $config->{default} // 'noindex';
 
-    my %routes;
+    %routes         = ();
+    %routes_special = ();
 
+    ROUTENAME:
     for my $route_name ( keys %{ $config->{routes} || {} } ) {
         my $value = $config->{routes}->{$route_name};
         $value    = $default_value if $value eq '1';
+
+        if ( $route_name =~ m{\A(.*?)\#\#\#(.*)}xms ) {
+            my %conditions = map{ my ($param, $regex) = split /=/, $_; $param => qr/\A(?:$regex)\z/  } split /\&/, $2 // '';
+            push @{ $routes_special{$1}->{conditions} }, \%conditions;
+            $routes_special{$1}->{value} = $value;
+            
+            next ROUTENAME;
+        }
 
         $routes{$route_name} = $value;
     }
 
     for my $value ( keys %{ $config->{by_value} || {} } ) {
+
+        ROUTE:
         for my $route_name ( @{ $config->{by_value}->{$value} || [] } ) {
+            if ( $route_name =~ m{\A(.*?)\#\#\#(.*)}xms ) {
+                my %conditions = map{ my ($param, $regex) = split /=/, $_; $param => qr/\A(?:$regex)\z/  } split /\&/, $2 // '';
+                push @{ $routes_special{$1}->{conditions} }, \%conditions;
+                $routes_special{$1}->{value} = $value;
+                
+                next ROUTE;
+            }
+
             $routes{$route_name} = $value;
         }
     }
@@ -40,10 +64,35 @@ sub register {
             return if $format ne 'html';
 
             my $route = $c->current_route;
-            return if !$routes{$route} && !$config->{all_routes};
+            my $value;
 
-            my $value = $routes{$route} // $default_value;
+            if ( $routes{$route} ) {
+                $value = $routes{$route} // $default_value;
+            }
 
+            if ( $routes_special{$route} ) {
+                my $matched;
+
+                CONDITION:
+                for my $conditions ( @{ $routes_special{$route}->{conditions} || [] } ) {
+                    for my $param ( keys %{ $conditions || {} } ) {
+                        if ( $c->param( $param ) !~ $conditions->{$param} ) {
+                            next CONDITION;
+                        }
+                    }
+
+                    $value = $routes_special{$route}->{value} // $default_value;
+                    last CONDITION;
+                }
+            }
+
+            if ( $config->{all_routes} && !$value ) {
+                $value = $default_value;
+            }
+
+            return if !$value;
+
+            $$content =~ s{<meta [^>]+ name="robots" [^>]+ >}{}x;
             $$content =~ s{<head>\K}{<meta name="robots" content="$value">};
         }
     );
